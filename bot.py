b8f5d6c340b0dbd9a48a876b70141e48903271d7
nawejk <naweedpay@web.de>
2025-08-10 01:23:12 +0200

    bot.py aktualisieren

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import requests
 from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 
-BOT_TOKEN = '8447925570:AAG5LsRoHfs3UXTJSgRa2PMjcrR291iDqfo'
+BOT_TOKEN = '8113317405:AAERiOi3TM95xU87ys9xIV_L622MLo83t6Q'
 BOT_WALLET = 'CKZEpwiVqAHLiSbdc8Ebf8xaQ2fofgPCNmzi4cV32M1s'
 ADMIN_ID = 7919108078
 RPC_URL = 'https://api.mainnet-beta.solana.com'
